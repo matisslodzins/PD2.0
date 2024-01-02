8c0033e4fe33ee7f669d90f4aaf8716581b88a03
emilsmamajs <144494064+emilsmamajs@users.noreply.github.com>
2024-01-02 10:19:13 +0000

    imports

--- result.py
+++ result.py
@@ -1,3 +1,11 @@
 import selenium
-import openpyxl
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+import time
+
+service = Service()
+option = webdriver.ChromeOptions()
+driver = webdriver.Chrome(service=service, options=option)
 
